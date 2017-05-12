37a1a7e1688d5a3c821863e395261600407ac177
Charles Pinnix <cpinnix89@gmail.com>
2017-05-12 19:21:45 -0400

    Restructured components to match scss.

--- demo/src/html/base/index.jsx
+++ demo/src/html/base/index.jsx
@@ -1,11 +1,11 @@
 const React = require('react');
 const Page = require('../components/page');
 const Hero = require('../components/hero');
-const Heading = require('../components/heading');
-const Paragraph = require('../components/paragraph');
-const Link = require('../components/link');
-const List = require('../components/list');
-const Code = require('../components/code');
+const Heading = require('../components/typography/heading');
+const Paragraph = require('../components/typography/paragraph');
+const Link = require('../components/typography/link');
+const List = require('../components/typography/list');
+const Code = require('../components/typography/code');
 
 module.exports = () => (
   <Page>
