a92bd23ceca1a64cedfc7586445115eb62d5f9d4
Charles Pinnix <cpinnix89@gmail.com>
2017-05-23 19:37:52 -0400

    Image center block using image component.

--- src/react/blocks/image-center/index.jsx
+++ src/react/blocks/image-center/index.jsx
@@ -1,15 +1,12 @@
 const React = require('react');
+const Image = require('../../components/image');
 
 module.exports = ({ source, caption }) => (
   <div className="vs-block-image-center" data-script="vsImageCenter">
     <div className="vs-block-image-center-container">
       <div className="vs-block-image-center-row">
         <div className="vs-block-image-center-content">
-          <div className="vs-image-image">
-            <img
-              className="vs-image-source"
-              src={ source } />
-          </div>
+          <Image source={ source } />
           <div className="vs-spacer--2" />
           <p className="vs-block-image-center-caption">{ caption }</p>
         </div>
---
+++ src/react/components/image/index.js
@@ -0,0 +1,9 @@
+const React = require('react');
+
+module.exports = ({ source }) => (
+  <div className="vs-image-image">
+    <img
+      className="vs-image-source"
+      src={ source } />
+  </div>
+);
