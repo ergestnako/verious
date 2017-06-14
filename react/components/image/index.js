8b12e49c696a74a4ba96a492159d5ff47803d6cd
Charlie Pinnix <cpinnix@hugeinc.com>
2017-06-14 14:01:56 -0400

    Created compiled react code library.

---
+++ react/components/image/index.js
@@ -0,0 +1,1 @@
+"use strict";var React=require("react");module.exports=function(_ref){var source=_ref.source;return React.createElement("div",{className:"vs-image-image","data-script":"vsImage"},React.createElement("img",{className:"vs-image-source",src:source}))};
---
+++ react/components/rectangle/index.js
@@ -0,0 +1,1 @@
+'use strict';var React=require('react');function style(_ref){var width=_ref.width,height=_ref.height,color=_ref.color;return{width:width?width+'rem':'',height:height?height+'rem':'',backgroundColor:''||color}}module.exports=function(props){return React.createElement('div',{style:style(props)})};
