80d8063a1e1419cade87dc9e8d3d9132835a9a4b
Charlie Pinnix <cpinnix@hugeinc.com>
2017-07-11 16:06:23 -0400

    Image resizing to baseline working in React.

--- react/components/Image/index.js
+++ react/components/Image/index.js
@@ -1,5 +1,47 @@
-const React = require('react');
+/* global document, getComputedStyle */
 
-module.exports = ({ source }) => (
-  <img src={ source } className="vs-image-source" />
-);
+const React = require('react');
+const Measure = require('react-measure').default;
+
+function getSpacingUnit() {
+  return parseFloat(getComputedStyle(document.documentElement).fontSize);
+}
+
+class Image extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      dimensions: {
+        width: -1,
+        height: -1,
+      },
+    };
+  }
+
+  render() {
+    const { height } = this.state.dimensions;
+
+    return (
+      <Measure
+        bounds
+        onResize={(contentRect) => {
+          this.setState({ dimensions: contentRect.bounds });
+        }}
+      >
+        {({ measureRef }) => {
+          const spacingUnit = getSpacingUnit();
+          const newHeight = Math.floor(height / spacingUnit) * spacingUnit;
+
+          return (
+            <div className="vs-image-image" style={{ height: newHeight }}>
+              <img src={this.props.source} className="vs-image-source" ref={measureRef} />
+            </div>
+          );
+        }}
+      </Measure>
+
+    );
+  }
+}
+
+module.exports = Image;
