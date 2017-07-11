4d6c7385dc03c63dc387cf0870c3d05b0407578e
Charlie Pinnix <cpinnix@hugeinc.com>
2017-07-11 19:17:25 -0400

    Some front page documentation work.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 const React = require('react');
 const Page = require('../templates/Page');
-const Hero = require('../components/Hero');
 const Square = require('../../react/components/Square');
 const Container = require('../../react/components/Container');
 const Row = require('../../react/components/Row');
@@ -24,65 +23,71 @@ module.exports = () => (
         </Row>
       </Container>
     </Wrapper>
-    <Square size="10" />
-    <Container>
-      <Row>
-        <Column span={[6, 12, 16, 16]} push={[0, 0, 0, 0]}>
-          <Heading size="2">Installation</Heading>
-        </Column>
-      </Row>
-      <Square size="2" />
-      <Code>
-        {`
-npm install verious
-        `}
-      </Code>
-    </Container>
-    <Square size="10" />
-    <Container>
-      <Row>
-        <Column span={[6, 12, 16, 16]} push={[0, 0, 0, 0]}>
-          <Heading size="2">Core Principles</Heading>
-        </Column>
-      </Row>
-      <Square size="2" />
-      <Row>
-        <Column span={[6, 6, 8, 8]} push={[0, 0, 0, 0]}>
-          <Heading size="3">Opt-in styles.</Heading>
-          <Paragraph>All styles and mixins are name-spaced so designers can use what they want and drop what they do not.</Paragraph>
-          <Square size="2" />
-        </Column>
-        <Column span={[6, 6, 8, 8]} push={[0, 0, 0, 0]}>
-          <Heading size="3">Responsive layout.</Heading>
-          <Paragraph>Grid layout for phone, tablet, desktop, and large desktop.</Paragraph>
-          <Square size="2" />
-        </Column>
-      </Row>
-      <Row>
-        <Column span={[6, 6, 8, 8]} push={[0, 0, 0, 0]}>
-          <Heading size="3">Baseline grid.</Heading>
-          <Paragraph>All elements are aligned to the same vertical grid and a modular scale is used to ensure proper scale across all viewports.</Paragraph>
-          <Square size="2" />
-        </Column>
-        <Column span={[6, 6, 8, 8]} push={[0, 0, 0, 0]}>
-          <Heading size="3">Component based design.</Heading>
-          <Paragraph>Components encapsulate HTML, CSS, and javascript code into easy to use packages. Designers can use components to build webpages without getting into the weeds.</Paragraph>
-          <Square size="2" />
-        </Column>
-      </Row>
-      <Row>
-        <Column span={[6, 6, 8, 8]} push={[0, 0, 0, 0]}>
-          <Heading size="3">Reusability.</Heading>
-          <Paragraph>Complex components can be isolated into a single file and included throughout a codebase.</Paragraph>
-          <Square size="2" />
-        </Column>
-        <Column span={[6, 6, 8, 8]} push={[0, 0, 0, 0]}>
-          <Heading size="3">Abstract Measurements.</Heading>
-          <Paragraph>The horizontal responsive grid and vertical baseline grid provide a set of abstract measurements to simplify design discussions and ensure responsiveness.</Paragraph>
-          <Square size="2" />
-        </Column>
-      </Row>
-    </Container>
-    <Square size="10" />
+    <Wrapper padding={[8, 0, 8, 0]}>
+      <Container>
+        <Row>
+          <Column span={[6, 12, 16, 16]} push={[0, 0, 0, 0]}>
+            <Heading size="2">Mission</Heading>
+            <Paragraph>To provide a set of easy to use <a href="https://facebook.github.io/react/">React</a> components for designing static websites with <a href="https://www.gatsbyjs.org/">Gatsby</a>.</Paragraph>
+          </Column>
+        </Row>
+        <Row>
+          <Column span={[6, 12, 16, 16]} push={[0, 0, 0, 0]}>
+            <Heading size="2">Core Principles</Heading>
+          </Column>
+        </Row>
+        <Row>
+          <Column span={[6, 6, 8, 8]} push={[0, 0, 0, 0]}>
+            <Heading size="3">Opt-in styles.</Heading>
+            <Paragraph>All styles and mixins are name-spaced so designers can use what they want and drop what they do not.</Paragraph>
+            <Square size="2" />
+          </Column>
+          <Column span={[6, 6, 8, 8]} push={[0, 0, 0, 0]}>
+            <Heading size="3">Responsive layout.</Heading>
+            <Paragraph>Grid layout for phone, tablet, desktop, and large desktop.</Paragraph>
+            <Square size="2" />
+          </Column>
+        </Row>
+        <Row>
+          <Column span={[6, 6, 8, 8]} push={[0, 0, 0, 0]}>
+            <Heading size="3">Baseline grid.</Heading>
+            <Paragraph>All elements are aligned to the same vertical grid and a modular scale is used to ensure proper scale across all viewports.</Paragraph>
+            <Square size="2" />
+          </Column>
+          <Column span={[6, 6, 8, 8]} push={[0, 0, 0, 0]}>
+            <Heading size="3">Component based design.</Heading>
+            <Paragraph>Components encapsulate HTML, CSS, and javascript code into easy to use packages. Designers can use components to build webpages without getting into the weeds.</Paragraph>
+            <Square size="2" />
+          </Column>
+        </Row>
+        <Row>
+          <Column span={[6, 6, 8, 8]} push={[0, 0, 0, 0]}>
+            <Heading size="3">Reusability.</Heading>
+            <Paragraph>Complex components can be isolated into a single file and included throughout a codebase.</Paragraph>
+            <Square size="2" />
+          </Column>
+          <Column span={[6, 6, 8, 8]} push={[0, 0, 0, 0]}>
+            <Heading size="3">Abstract Measurements.</Heading>
+            <Paragraph>The horizontal responsive grid and vertical baseline grid provide a set of abstract measurements to simplify design discussions and ensure responsiveness.</Paragraph>
+            <Square size="2" />
+          </Column>
+        </Row>
+        <Row>
+          <Column span={[6, 12, 16, 16]} push={[0, 0, 0, 0]}>
+            <Heading size="2">Installation</Heading>
+            <Code>
+              {`
+  npm install verious
+              `}
+            </Code>
+          </Column>
+        </Row>
+        <Row>
+          <Column span={[6, 12, 16, 16]} push={[0, 0, 0, 0]}>
+            <Heading size="2">Usage</Heading>
+          </Column>
+        </Row>
+      </Container>
+    </Wrapper>
   </Page>
 );
