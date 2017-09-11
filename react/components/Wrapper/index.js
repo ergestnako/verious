851844ea1b8849e29f88da4ed46a3f56ed51e70e
Charlie Pinnix <cpinnix@hugeinc.com>
2017-09-11 14:10:15 -0400

    Added visibility property to Wrapper component.

--- react/components/Wrapper/index.js
+++ react/components/Wrapper/index.js
@@ -1,7 +1,7 @@
 const React = require('react');
 const utilities = require('../../utilities');
 
-function getClassName({ backgroundColor, padding }) {
+function getClassName({ backgroundColor, padding, visibility }) {
   let classes = [];
 
   if (backgroundColor) {
@@ -20,6 +20,32 @@ function getClassName({ backgroundColor, padding }) {
     ];
   }
 
+  if (visibility) {
+    classes = [
+      ...classes,
+      `vs-padding-top-${padding[0]}`,
+      `vs-padding-right-${padding[1]}`,
+      `vs-padding-bottom-${padding[2]}`,
+      `vs-padding-left-${padding[3]}`,
+    ];
+
+    if (visibility[0] === false) {
+      classes = [...classes, 'vs-hidden-sm'];
+    }
+
+    if (visibility[1] === false) {
+      classes = [...classes, 'vs-hidden-md'];
+    }
+
+    if (visibility[2] === false) {
+      classes = [...classes, 'vs-hidden-lg'];
+    }
+
+    if (visibility[3] === false) {
+      classes = [...classes, 'vs-hidden-xl'];
+    }
+  }
+
   return classes.join(' ');
 }
 
@@ -35,11 +61,10 @@ function getStyles({ style, backgroundColor }) {
   return Object.assign({}, style, styles);
 }
 
-module.exports = ({ children, style, backgroundColor, padding }) => (
-  <div
+module.exports = ({ children, style, backgroundColor, padding, visibility }) =>
+  (<div
     style={getStyles({ style, backgroundColor })}
-    className={getClassName({ backgroundColor, padding })}
+    className={getClassName({ backgroundColor, padding, visibility })}
   >
-    { children }
-  </div>
-);
+    {children}
+  </div>);
