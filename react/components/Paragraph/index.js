b727d5d281da397175254913291c653482fa3f5b
Charles Pinnix <cpinnix89@gmail.com>
2017-09-19 19:12:10 -0500

    Cleaned up heading styles.

--- react/components/Paragraph/index.js
+++ react/components/Paragraph/index.js
@@ -1,47 +1,17 @@
 const React = require('react');
-const utilities = require('../../utilities');
-
-function getClassName({ backgroundColor, textColor }) {
-  const classes = ['vs-p'];
-
-  if (backgroundColor) {
-    if (utilities.isInternalColor(backgroundColor)) {
-      classes.push(`vs-background-color--${backgroundColor.slice(3)}`);
-    }
-  }
-
-  if (textColor) {
-    if (utilities.isInternalColor(textColor)) {
-      classes.push(`vs-text-color--${textColor.slice(3)}`);
-    }
-  }
-
-  return classes.join(' ');
-}
-
-function getStyles({ style, backgroundColor, textColor }) {
-  const styles = {};
-
-  if (backgroundColor) {
-    if (!utilities.isInternalColor(backgroundColor)) {
-      styles.backgroundColor = backgroundColor;
-    }
-  }
-
-  if (textColor) {
-    if (!utilities.isInternalColor(textColor)) {
-      styles.color = textColor;
-    }
-  }
-
-  return Object.assign({}, style, styles);
-}
+const Text = require('../Text');
 
 module.exports = ({ children, style, size, textColor }) => (
   <p
-    className={getClassName({ children, size, textColor })}
-    style={getStyles({ children, style, size, textColor })}
+    className="vs-p"
   >
-    { children }
+    <Text
+      style={style}
+      size={1}
+      font="helvetica"
+      color={textColor}
+    >
+      {children}
+    </Text>
   </p>
 );
--- react/components/Text/index.js
+++ react/components/Text/index.js
@@ -1,17 +1,41 @@
 const React = require('react');
 const _ = require('lodash');
+const utilities = require('../../utilities');
 
 const fonts = ['montserrat'];
 
-function getClassName({ size, font }) {
-  let className = '';
+function getClassName({ size, font, color }) {
+  let classes = [];
 
   if (_.findIndex(fonts, font)) {
-    className = `vs-type-${font}--${size}`;
+    classes = [...classes, `vs-text-${font}--${size}`];
   }
 
-  return className;
+  if (color) {
+    if (utilities.isInternalColor(color)) {
+      classes = [...classes, `vs-text-color--${color.slice(3)}`];
+    }
+  }
+
+  return classes.join(' ');
 }
 
-module.exports = ({ size, font }) =>
-  <div className={getClassName({ size, font })}>Hello World</div>;
+function getStyles({ style, color }) {
+  const styles = {};
+
+  if (color) {
+    if (!utilities.isInternalColor(color)) {
+      styles.color = color;
+    }
+  }
+
+  return Object.assign({}, style, styles);
+}
+
+module.exports = ({ children, style, size, font, color }) =>
+  (<div
+    className={getClassName({ size, font, color })}
+    style={getStyles({ style, size, font, color })}
+  >
+    {children}
+  </div>);
