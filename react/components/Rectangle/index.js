3d159749bb24c05be48d635f5c439bb76c66d9da
Charlie Pinnix <cpinnix@hugeinc.com>
2017-09-22 13:26:05 -0400

    Simplified Square component to use Rectangle component.

--- react/components/Rectangle/index.js
+++ react/components/Rectangle/index.js
@@ -1,35 +1,19 @@
 const React = require('react');
+const BackgroundColor = require('../BackgroundColor');
 
-function getClassName({ backgroundColor }) {
-  const classes = [];
-
-  if (backgroundColor) {
-    if (backgroundColor.includes('vs-')) {
-      classes.push(`vs-background-color--${backgroundColor.slice(3)}`);
-    }
-  }
-
-  return classes.join(' ');
-}
-
-function getStyles({ width, height, backgroundColor }) {
+function getStyles({ width, height }) {
   const styles = {
     width: width ? `${width}rem` : '',
     height: height ? `${height}rem` : '',
   };
 
-  if (backgroundColor) {
-    if (!backgroundColor.includes('vs-')) {
-      styles.backgroundColor = backgroundColor;
-    }
-  }
-
   return styles;
 }
 
-module.exports = (props) => (
-  <div
-    style={getStyles(props)}
-    className={getClassName(props)}
-  />
+module.exports = props => (
+  <div style={{ display: 'inline-block' }}>
+    <BackgroundColor color={props.backgroundColor}>
+      <div style={getStyles(props)} />
+    </BackgroundColor>
+  </div>
 );
--- react/components/Square/index.js
+++ react/components/Square/index.js
@@ -1,21 +1,10 @@
 const React = require('react');
-const BackgroundColor = require('../BackgroundColor');
-
-function getStyles({ size, style }) {
-  const styles = {
-    width: `${size}rem`,
-    height: `${size}rem`,
-  };
-
-  return Object.assign({}, style, styles);
-}
+const Rectangle = require('../Rectangle');
 
 module.exports = props => (
-  <div style={{ display: 'inline-block' }}>
-    <BackgroundColor color={props.backgroundColor}>
-      <div
-        style={getStyles(props)}
-      />
-    </BackgroundColor>
-  </div>
+  <Rectangle
+    width={props.size}
+    height={props.size}
+    backgroundColor={props.backgroundColor}
+  />
 );
