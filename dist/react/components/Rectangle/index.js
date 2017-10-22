01348d0dc0b4e04bed29356663f13c3d3627575a
Charles Pinnix <cpinnix89@gmail.com>
2017-10-22 19:59:12 -0400

    Updated distribution files. Bumped version.

--- dist/react/components/Rectangle/index.js
+++ dist/react/components/Rectangle/index.js
@@ -1,18 +1,33 @@
 'use strict';
 
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
 var React = require('react');
 var BackgroundColor = require('../BackgroundColor');
 
-function getStyles(_ref) {
+function getClassName(_ref) {
   var width = _ref.width,
       height = _ref.height;
 
-  var styles = {
-    width: width ? width + 'rem' : '',
-    height: height ? height + 'rem' : ''
-  };
+  var classes = [];
 
-  return styles;
+  if (width) {
+    if (Array.isArray(width)) {
+      classes = [].concat(_toConsumableArray(classes), ['vs-width-sm-' + width[0], 'vs-width-md-' + width[1], 'vs-width-lg-' + width[2], 'vs-width-xl-' + width[3]]);
+    } else {
+      classes = [].concat(_toConsumableArray(classes), ['vs-width-' + width]);
+    }
+  }
+
+  if (height) {
+    if (Array.isArray(width)) {
+      classes = [].concat(_toConsumableArray(classes), ['vs-height-sm-' + width[0], 'vs-height-md-' + width[1], 'vs-height-lg-' + width[2], 'vs-height-xl-' + width[3]]);
+    } else {
+      classes = [].concat(_toConsumableArray(classes), ['vs-height-' + width]);
+    }
+  }
+
+  return classes.join(' ');
 }
 
 module.exports = function (props) {
@@ -22,7 +37,7 @@ module.exports = function (props) {
     React.createElement(
       BackgroundColor,
       { color: props.backgroundColor },
-      React.createElement('div', { style: getStyles(props) })
+      React.createElement('div', { className: getClassName(props) })
     )
   );
 };
