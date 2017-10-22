18e79624e0c960806c3f72a4e70474b0146e5464
Charles Pinnix <cpinnix89@gmail.com>
2017-10-22 19:12:40 -0400

    Updated distribution files. Bumped version.

--- dist/react/components/Image/index.js
+++ dist/react/components/Image/index.js
@@ -8,18 +8,9 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-/* global document, getComputedStyle */
-
 var React = require('react');
-var Measure = require('react-measure').default;
-
-function getSpacingUnit() {
-  if (typeof getComputedStyle === 'undefined') {
-    return 0;
-  }
-
-  return parseFloat(getComputedStyle(document.documentElement).fontSize);
-}
+var Wrapper = require('../Wrapper');
+var ImageBaseline = require('../ImageBaseline');
 
 var Image = function (_React$Component) {
   _inherits(Image, _React$Component);
@@ -27,60 +18,44 @@ var Image = function (_React$Component) {
   function Image() {
     _classCallCheck(this, Image);
 
-    var _this = _possibleConstructorReturn(this, (Image.__proto__ || Object.getPrototypeOf(Image)).call(this));
-
-    _this.state = {
-      loaded: false,
-      dimensions: {
-        width: -1,
-        height: -1
-      }
-    };
-
-    _this.handleLoad = _this.handleLoad.bind(_this);
-    return _this;
+    return _possibleConstructorReturn(this, (Image.__proto__ || Object.getPrototypeOf(Image)).apply(this, arguments));
   }
 
   _createClass(Image, [{
-    key: 'handleLoad',
-    value: function handleLoad() {
-      this.setState(Object.assign({}, this.state, { loaded: true }));
-    }
-  }, {
     key: 'render',
     value: function render() {
-      var _this2 = this;
+      var element = React.createElement('div', null);
 
-      var height = this.state.dimensions.height;
+      if (Array.isArray(this.props.source)) {
+        element = React.createElement(
+          Wrapper,
+          null,
+          React.createElement(
+            Wrapper,
+            { visibility: [true, false, false, false] },
+            React.createElement(ImageBaseline, { source: this.props.source[0] })
+          ),
+          React.createElement(
+            Wrapper,
+            { visibility: [false, true, false, false] },
+            React.createElement(ImageBaseline, { source: this.props.source[1] })
+          ),
+          React.createElement(
+            Wrapper,
+            { visibility: [false, false, true, false] },
+            React.createElement(ImageBaseline, { source: this.props.source[2] })
+          ),
+          React.createElement(
+            Wrapper,
+            { visibility: [false, false, false, true] },
+            React.createElement(ImageBaseline, { source: this.props.source[3] })
+          )
+        );
+      } else {
+        element = React.createElement(ImageBaseline, { source: this.props.source });
+      }
 
-
-      return React.createElement(
-        Measure,
-        {
-          bounds: true,
-          onResize: function onResize(contentRect) {
-            _this2.setState({ dimensions: contentRect.bounds });
-          }
-        },
-        function (_ref) {
-          var measureRef = _ref.measureRef;
-
-          var spacingUnit = getSpacingUnit();
-          var newHeight = Math.floor(height / spacingUnit) * spacingUnit;
-
-          return React.createElement(
-            'div',
-            { className: 'vs-image-image', style: { height: newHeight } },
-            React.createElement('img', {
-              src: _this2.props.source,
-              alt: _this2.props.source,
-              className: _this2.state.loaded ? 'vs-image-source' : '',
-              ref: measureRef,
-              onLoad: _this2.handleLoad
-            })
-          );
-        }
-      );
+      return element;
     }
   }]);
 
---
+++ dist/react/components/ImageBaseline/Index.js
@@ -0,0 +1,90 @@
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+/* global document, getComputedStyle */
+
+var React = require('react');
+var Measure = require('react-measure').default;
+
+function getSpacingUnit() {
+  if (typeof getComputedStyle === 'undefined') {
+    return 0;
+  }
+
+  return parseFloat(getComputedStyle(document.documentElement).fontSize);
+}
+
+var ImageBaseline = function (_React$Component) {
+  _inherits(ImageBaseline, _React$Component);
+
+  function ImageBaseline() {
+    _classCallCheck(this, ImageBaseline);
+
+    var _this = _possibleConstructorReturn(this, (ImageBaseline.__proto__ || Object.getPrototypeOf(ImageBaseline)).call(this));
+
+    _this.state = {
+      loaded: false,
+      dimensions: {
+        width: -1,
+        height: -1
+      }
+    };
+
+    _this.handleLoad = _this.handleLoad.bind(_this);
+    return _this;
+  }
+
+  _createClass(ImageBaseline, [{
+    key: 'handleLoad',
+    value: function handleLoad() {
+      this.setState(Object.assign({}, this.state, { loaded: true }));
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      var _this2 = this;
+
+      var height = this.state.dimensions.height;
+
+
+      return React.createElement(
+        Measure,
+        {
+          bounds: true,
+          onResize: function onResize(contentRect) {
+            _this2.setState({ dimensions: contentRect.bounds });
+          }
+        },
+        function (_ref) {
+          var measureRef = _ref.measureRef;
+
+          var spacingUnit = getSpacingUnit();
+          var newHeight = Math.floor(height / spacingUnit) * spacingUnit;
+
+          return React.createElement(
+            'div',
+            { className: 'vs-image-image', style: { height: newHeight } },
+            React.createElement('img', {
+              src: _this2.props.source,
+              alt: _this2.props.source,
+              className: _this2.state.loaded ? 'vs-image-source' : '',
+              ref: measureRef,
+              onLoad: _this2.handleLoad
+            })
+          );
+        }
+      );
+    }
+  }]);
+
+  return ImageBaseline;
+}(React.Component);
+
+module.exports = ImageBaseline;
