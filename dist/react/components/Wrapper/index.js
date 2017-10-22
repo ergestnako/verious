06d2560a5cb41cd10365ebf7c829025252a8ac84
Charles Pinnix <cpinnix89@gmail.com>
2017-10-22 18:36:59 -0400

    Updated distribution files and bumped version.

--- dist/react/components/Wrapper/index.js
+++ dist/react/components/Wrapper/index.js
@@ -4,19 +4,47 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
 
 var React = require('react');
 
+var edges = ['top', 'right', 'bottom', 'left'];
+
 function getClassName(_ref) {
   var padding = _ref.padding,
-      visibility = _ref.visibility,
-      pull = _ref.pull;
+      margin = _ref.margin,
+      pull = _ref.pull,
+      visibility = _ref.visibility;
 
   var classes = [];
 
   if (padding) {
-    classes = [].concat(_toConsumableArray(classes), ['vs-padding-top-' + padding[0], 'vs-padding-right-' + padding[1], 'vs-padding-bottom-' + padding[2], 'vs-padding-left-' + padding[3]]);
+    for (var i = 0; i < padding.length; i += 1) {
+      var p = padding[i];
+      if (typeof p === 'number') {
+        classes = [].concat(_toConsumableArray(classes), ['vs-padding-' + edges[i] + '-' + p]);
+      } else if (Array.isArray(p)) {
+        classes = [].concat(_toConsumableArray(classes), ['vs-sm-padding-' + edges[i] + '-' + p[0], 'vs-md-padding-' + edges[i] + '-' + p[1], 'vs-lg-padding-' + edges[i] + '-' + p[2], 'vs-xl-padding-' + edges[i] + '-' + p[3]]);
+      }
+    }
+  }
+
+  if (margin) {
+    for (var _i = 0; _i < padding.length; _i += 1) {
+      var _p = margin[_i];
+      if (typeof _p === 'number') {
+        classes = [].concat(_toConsumableArray(classes), ['vs-margin-' + edges[_i] + '-' + _p]);
+      } else if (Array.isArray(_p)) {
+        classes = [].concat(_toConsumableArray(classes), ['vs-sm-margin-' + edges[_i] + '-' + _p[0], 'vs-md-margin-' + edges[_i] + '-' + _p[1], 'vs-lg-margin-' + edges[_i] + '-' + _p[2], 'vs-xl-margin-' + edges[_i] + '-' + _p[3]]);
+      }
+    }
   }
 
   if (pull) {
-    classes = [].concat(_toConsumableArray(classes), ['vs-pull-top-' + pull[0], 'vs-pull-right-' + pull[1], 'vs-pull-bottom-' + pull[2], 'vs-pull-left-' + pull[3]]);
+    for (var _i2 = 0; _i2 < padding.length; _i2 += 1) {
+      var _p2 = pull[_i2];
+      if (typeof _p2 === 'number') {
+        classes = [].concat(_toConsumableArray(classes), ['vs-pull-' + edges[_i2] + '-' + _p2]);
+      } else if (Array.isArray(_p2)) {
+        classes = [].concat(_toConsumableArray(classes), ['vs-sm-pull-' + edges[_i2] + '-' + _p2[0], 'vs-md-pull-' + edges[_i2] + '-' + _p2[1], 'vs-lg-pull-' + edges[_i2] + '-' + _p2[2], 'vs-xl-pull-' + edges[_i2] + '-' + _p2[3]]);
+      }
+    }
   }
 
   if (visibility) {
@@ -44,8 +72,9 @@ module.exports = function (_ref2) {
   var children = _ref2.children,
       style = _ref2.style,
       padding = _ref2.padding,
-      visibility = _ref2.visibility,
-      pull = _ref2.pull;
+      margin = _ref2.margin,
+      pull = _ref2.pull,
+      visibility = _ref2.visibility;
   return React.createElement(
     'div',
     { style: style, className: getClassName({ padding: padding, visibility: visibility, pull: pull }) },
