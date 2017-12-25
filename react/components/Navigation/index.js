ee17362797d200bdaf32e8b992f90ad678ac8d2d
Charles Pinnix <cpinnix89@gmail.com>
2017-12-25 10:45:10 -0500

    Upgraded navigation interaction.

--- react/components/Navigation/index.js
+++ react/components/Navigation/index.js
@@ -8,26 +8,39 @@ const Spacer = require('../Spacer');
 const BackgroundColor = require('../BackgroundColor');
 const IconButton = require('../IconButton');
 const IconMenu = require('../IconMenu');
+const Measure = require('react-measure').default;
 
 class Navigation extends React.Component {
   constructor() {
     super();
-    this.state = { open: false };
+    this.state = {
+      open: false,
+      dimensions: {
+        width: -1,
+        height: -1
+      }
+    };
   }
 
   render() {
+    const { height } = this.state.dimensions;
+
     return (
       <BackgroundColor color={this.props.backgroundColor || 'vs-white'}>
-        <Wrapper padding={[1, 0, 1, 0]}>
+        <Wrapper>
           <Container>
             <Row>
               <Column span={[12, 12, 12, 12, 12]}>
-                <Wrapper padding={[0, 0, 0, 0]}>
+                <Wrapper padding={[1, 0, 1, 0]}>
                   <Layout direction="horizontal">
                     {this.props.left}
                     <Spacer />
                     <IconButton
-                      onClick={() => this.setState({ open: !this.state.open })}
+                      onClick={() => {
+                        this.setState({
+                          open: !this.state.open
+                        });
+                      }}
                     >
                       <IconMenu
                         iconColor={this.props.iconColor || 'vs-white'}
@@ -39,16 +52,33 @@ class Navigation extends React.Component {
               </Column>
             </Row>
           </Container>
-          <div style={{ display: this.state.open ? '' : 'none' }}>
-            <Wrapper padding={[2, 0, 2, 0]}>
-              <Container>
-                <Row>
-                  <Column span={[12, 12, 12, 12, 12]}>
-                    {this.props.children}
-                  </Column>
-                </Row>
-              </Container>
-            </Wrapper>
+          <div
+            style={{
+              overflow: 'hidden',
+              height: this.state.open ? height : 0,
+              transition: 'height 200ms linear'
+            }}
+          >
+            <Measure
+              bounds
+              onResize={contentRect => {
+                this.setState({ dimensions: contentRect.bounds });
+              }}
+            >
+              {({ measureRef }) => (
+                <div ref={measureRef}>
+                  <Wrapper padding={[2, 0, 2, 0]}>
+                    <Container>
+                      <Row>
+                        <Column span={[12, 12, 12, 12, 12]}>
+                          {this.props.children}
+                        </Column>
+                      </Row>
+                    </Container>
+                  </Wrapper>
+                </div>
+              )}
+            </Measure>
           </div>
         </Wrapper>
       </BackgroundColor>
