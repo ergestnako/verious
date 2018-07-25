e29a72db2049fad5a46476c327d83241f1dc24b9
Charles Pinnix <cpinnix89@gmail.com>
2018-07-24 22:19:03 -0400

    Added manifest plugin.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,18 @@
 module.exports = {
   plugins: [
     "gatsby-plugin-sass",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "The Narrow Sea",
+        short_name: "The Narrow Sea",
+        start_url: "/",
+        background_color: "#FFFFFF",
+        theme_color: "#000000",
+        display: "minimal-ui",
+        icon: "static/img/favicon-32x32.png"
+      }
+    },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-offline`,
     `gatsby-plugin-netlify`
